0915c475811fb974d31043b541622e0a64684e49
Giuseppe Galtieri <giuseppe.galtieri@enhancers.it>
2022-10-28 17:59:04 +0200

    added eslint - prettier - editorConfig

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # playing-with-react
 The goal of the project is to test new react features and libraries
+
+## Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+yarn
+yarn dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useMemo, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const s = useMemo(() => {
+    return count;
+  }, []);
 
   return (
     <div className="App">
+      {s}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -17,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -28,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
