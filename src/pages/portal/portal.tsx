39b006d374b01cb1b14b030ac32b6e1ecaf819f6
Giuseppe Galtieri <giuseppe.galtieri@enhancers.it>
2022-11-02 14:31:20 +0100

    added button to show the portal

--- src/pages/portal/portal.tsx
+++ src/pages/portal/portal.tsx
@@ -1,18 +1,33 @@
 import { FunctionComponent, useState } from 'react';
 
 import Modal from './modal/modal';
+import ReactPortal from './react-portal';
 
 interface Props {}
 
 const PortalPage: FunctionComponent<Props> = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenPortal, setIsOpenPortal] = useState(false);
 
   return (
     <div className="portal-page">
-      <button onClick={() => setIsOpen(true)}>Click to Open Modal</button>
-      <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
-        This is Modal Content!
-      </Modal>
+      <ReactPortal wrapperId="portal-test">
+        <button
+          onClick={() => setIsOpenPortal(isOpen => !isOpen)}
+          style={{ position: 'fixed', bottom: '4vh', right: '4vw' }}
+        >
+          {isOpenPortal ? 'hide' : 'show'} portal
+        </button>
+      </ReactPortal>
+
+      {isOpenPortal ? (
+        <>
+          <button onClick={() => setIsOpen(true)}>Click to Open Modal</button>
+          <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
+            This is Modal Content!
+          </Modal>
+        </>
+      ) : null}
     </div>
   );
 };
