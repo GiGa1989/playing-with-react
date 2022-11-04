b84d8237556a2faa06c0d8818385b8c133c10b5b
Giuseppe Galtieri <giuseppe.galtieri@enhancers.it>
2022-11-04 17:32:12 +0100

    added persist and devtool config

--- src/pages/zustand/zustand2.tsx
+++ src/pages/zustand/zustand2.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
-import create, { StateCreator, useStore } from 'zustand';
+import create, { StateCreator } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
 interface BearSlice {
   bears: number;
   addBear: () => void;
   eatFish: () => void;
 }
+
 const createBearSlice: StateCreator<
   BearSlice & FishSlice,
   [],
@@ -14,7 +16,9 @@ const createBearSlice: StateCreator<
 > = set => ({
   bears: 0,
   addBear: () => set(state => ({ bears: state.bears + 1 })),
-  eatFish: () => set(state => ({ fishes: state.fishes - 1 })),
+  eatFish: () =>
+    //@ts-ignore
+    set(state => ({ fishes: state.fishes - 1 }), false, 'bear/eatFish'),
 });
 
 interface FishSlice {
@@ -35,17 +39,38 @@ const createFishSlice: StateCreator<
   addFish: () => set(state => ({ fishes: state.fishes + 1 })),
 });
 
-const useBoundStore = create<BearSlice & FishSlice>()((...a) => ({
+/*
+  // prima
+  const useBoundStore = create<BearSlice & FishSlice>()((...a) => ({
+    ...createBearSlice(...a),
+    ...createFishSlice(...a),
+  }));
+*/
+
+// const myMiddlewares = f => devtools(persist(f));
+
+let store = (...a) => ({
   ...createBearSlice(...a),
   ...createFishSlice(...a),
-}));
+});
+
+store = persist(store, {
+  name: 'bear-fish', // unique name, può essere un nome diverso
+  getStorage: () => localStorage, // (optional) by default, 'localStorage' is used
+});
+store = devtools(store);
+// const useStore = create(store);
+
+let useBoundStore = create<BearSlice & FishSlice>()(store);
 
 const ZustandPage2: FC = () => {
+  // const bears2 = useBoundStore(state => state.bears);
   const { bears, fishes, addFish, addBear, eatFish } = useBoundStore();
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <span>{fishes} fishes</span>
       <span>{bears} bears</span>
+      {/* <span>{bears2} bears2</span> */}
       <button onClick={addFish}>addFish</button>
       <button onClick={addBear}>addBear</button>
       <button onClick={eatFish}>eatFish</button>
