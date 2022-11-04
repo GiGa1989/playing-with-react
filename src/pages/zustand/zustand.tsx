3c5cc83a9486e3408f1f01bbb5902b3e6da603d8
Giuseppe Galtieri <giuseppe.galtieri@enhancers.it>
2022-11-04 15:27:27 +0100

    added persist

--- src/pages/zustand/zustand.tsx
+++ src/pages/zustand/zustand.tsx
@@ -1,10 +1,23 @@
 import { FC } from 'react';
 import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
 
-const useStore = create(set => ({
+
+/*
+  // basic store
+  const useStore = create(set => ({
+    count: 1,
+    inc: () => set(state => ({ count: state.count + 1 })),
+  }));
+*/
+
+let store = set => ({
   count: 1,
   inc: () => set(state => ({ count: state.count + 1 })),
-}));
+});
+
+store = persist(store, { name: 'count' });
+const useStore = create(store);
 
 function Counter() {
   const { count, inc } = useStore();
@@ -16,15 +29,7 @@ function Counter() {
   );
 }
 const ZustandPage: FC = () => {
-  return (
-    <div className="main">
-      <div className="code">
-        <div className="code-container">
-          <Counter />
-        </div>
-      </div>
-    </div>
-  );
+  return <Counter />;
 };
 
 export default ZustandPage;
